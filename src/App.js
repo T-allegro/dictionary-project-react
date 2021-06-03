dea3c8d4ef0a7a016eac55ca0e7bfbd717160232
T-allegro <78311737+T-allegro@users.noreply.github.com>
2021-06-03 09:28:43 +0100

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+      <header className="App-header">
+        <h1>
+         DICTIONARY
+       </h1>
+      </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
