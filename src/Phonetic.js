450734b59676b7c6ec1878335c78974fa91d840e
T-allegro <78311737+T-allegro@users.noreply.github.com>
2021-06-07 10:47:40 +0100

    Added phonetic icon

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props){
     return (
         <div className="Phonetic">
             <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
-                Listen
+                <i class="fas fa-volume-up"></i>
             </a>
             {props.phonetic.text}
         </div>
