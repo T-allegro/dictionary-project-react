caaff5ad4baf5b5b6d429b747e2ed8c08fb26ed7
T-allegro <78311737+T-allegro@users.noreply.github.com>
2021-06-07 11:54:35 +0100

    Phonetic audio_change test

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,26 @@
 import React from "react";
+import ReactAudioPlayer from "react-audio-player";
 import "./Phonetic.css";
 
 export default function Phonetic(props){
+    function handleClick(){
+        return (
+        <ReactAudioPlayer 
+            src={props.phonetic.audio}
+        />
+        )
+    }
+
     return (
         <div className="Phonetic">
-            <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
-                <i class="fas fa-volume-up"></i>
-            </a>
+            <button onClick={handleClick}>
+            <i class="fas fa-volume-up"></i>
+            </button>
             {props.phonetic.text}
         </div>
     )
 }
+
+// <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
+// <i class="fas fa-volume-up"></i>
+// </a>
